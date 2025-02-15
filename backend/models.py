eddd2d41e7a80d6aaca0d83932568fd87e9c628a
youssof20 <youssofsallam25@gmail.com>
2025-02-15 12:31:58 +0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     priority = db.Column(db.Integer, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {"id": self.id, "name": self.name, "priority": self.priority, "duration": self.duration}
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(100), nullable=False)
+    data = db.Column(db.Text, nullable=False)
